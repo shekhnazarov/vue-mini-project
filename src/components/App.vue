8594858739afbd66fda62693115592d3271fc862
shekhnazarov <umarshexnazarov@gmail.com>
2023-10-17 17:51:44 +0500

    Structure is finished

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <div class="content">
+      <AppInfo />
+      <AppFilter />
+    </div>
+  </div>
+</template>
+<script>
+import AppFilter from './AppFilter.vue'
+import AppInfo from './AppInfo.vue'
+
+export default { components: { AppInfo, AppFilter } }
+</script>
+<style>
+.app {
+  height: 100vh;
+  color: #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: blanchedalmond;
+}
+
+.content {
+  width: 1000px;
+  min-height: 700px;
+  background-color: #fff;
+}
+</style>
---
+++ src/components/AppInfo.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <div>AppInfo</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang=""></style>
